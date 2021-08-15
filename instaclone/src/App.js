2036b92a9023e87158fdaf54153131a900918089
vicky <srivicky2000@gmail.com>
2021-08-15 15:19:07 +0530

    ui working

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -6,40 +6,62 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
 
-
+import Story from 'react-native-story'
 
 const App = () => {
 
-
+  const stories = [
+    {
+      id: "4",
+      source: require("./asset/one.jpg"),
+      user: "Ugur Erdal",
+      avatar: require("./asset/three.jpg")
+    },
+    {
+      id: "2",
+      source: require("./asset/four.jpg"),
+      user: "Mustafa",
+      avatar: require("./asset/seven.jpg")
+    },
+    {
+      id: "5",
+      source: require("./asset/six.jpg"),
+      user: "Emre Yilmaz",
+      avatar: require("./asset/two.jpg")
+    },
+    {
+      id: "3",
+      source: require("./asset/seven.jpg"),
+      user: "Cenk Gun",
+      avatar: require("./asset/seven.jpg")
+    },
+  ];
 
   return (
-    <SafeAreaView>
-      
-    </SafeAreaView>
+    <View>
+      <Story
+            unPressedBorderColor="#e95950"
+            pressedBorderColor="#ebebeb"
+            stories={stories}
+            footerComponent={
+                <TextInput
+                    placeholder="Send message"
+                    placeholderTextColor="white"
+                    style={styles.footerInput}
+                />
+            }
+        />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+
 });
 
 export default App;
